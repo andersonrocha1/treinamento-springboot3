1dba2e71fa11647171a8214356b7d1535aa32b2a
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-01-23 14:06:46 -0300

    Projeto Inicial criado

---
+++ src/main/java/com/andersondev/treinaspringboot/TreinaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.andersondev.treinaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinaspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersondev/treinaspringboot/TreinaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersondev.treinaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
