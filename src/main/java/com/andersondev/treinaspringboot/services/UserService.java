e18ef1685b374ba5428a3518c882c9b9140c4315
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-01-24 12:36:31 -0300

    Service layer, component registration

---
+++ src/main/java/com/andersondev/treinaspringboot/services/UserService.java
@@ -0,0 +1,33 @@
+package com.andersondev.treinaspringboot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersondev.treinaspringboot.entities.User;
+import com.andersondev.treinaspringboot.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		
+		return repository.findAll();
+	}
+	
+	
+	public User findById(Long id) {
+		
+	  Optional<User> obj = repository.findById(id);
+	  return obj.get();
+	  
+	}
+	
+	
+
+}
