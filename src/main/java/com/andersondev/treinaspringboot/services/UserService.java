b67dba3cb8f15ad84fac290111b39c484d651a03
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-01-28 13:33:36 -0300

    Exception handling - delete

--- src/main/java/com/andersondev/treinaspringboot/services/UserService.java
+++ src/main/java/com/andersondev/treinaspringboot/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.andersondev.treinaspringboot.entities.User;
 import com.andersondev.treinaspringboot.repositories.UserRepository;
+import com.andersondev.treinaspringboot.services.exceptions.DatabaseException;
 import com.andersondev.treinaspringboot.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -37,9 +40,13 @@ public class UserService {
 	
 	
 	public void delete(Long id) {
-		
+		try {
 		repository.deleteById(id);
-		
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	
