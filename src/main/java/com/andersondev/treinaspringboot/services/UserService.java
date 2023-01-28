284ef49bfc452ca670fd78b5e40589f538313997
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-01-28 12:29:15 -0300

    Exception handling - findById

--- src/main/java/com/andersondev/treinaspringboot/services/UserService.java
+++ src/main/java/com/andersondev/treinaspringboot/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.andersondev.treinaspringboot.entities.User;
 import com.andersondev.treinaspringboot.repositories.UserRepository;
+import com.andersondev.treinaspringboot.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -24,7 +25,7 @@ public class UserService {
 	public User findById(Long id) {
 		
 	  Optional<User> obj = repository.findById(id);
-	  return obj.get();
+	  return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	  
 	}
 	
---
+++ src/main/java/com/andersondev/treinaspringboot/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.andersondev.treinaspringboot.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException (Object id) {
+		
+		super("Resource not found. Id = " + id);
+		
+	}
+
+}
